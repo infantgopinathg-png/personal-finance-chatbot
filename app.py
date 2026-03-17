a8ad01ccf7c9fc6c56c93c024e12711db00ee54b
infantgopinathg-png <infantgopinathg@gmail.com>
2026-03-17 11:23:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -194,37 +194,14 @@ if st.button("Analyze Financial Plan"):
 
     st.subheader("Retirement Readiness")
 
-    if readiness_percent <= 30:
-        bar_color = "red"
-        needle_color = "darkred"
-    elif readiness_percent <= 59:
-        bar_color = "orange"
-        needle_color = "darkorange"
-    else:
-        bar_color = "green"
-        needle_color = "darkgreen"
-
     gauge = go.Figure(go.Indicator(
         mode="gauge+number",
         value=readiness_percent,
-        title={'text': "Readiness %"},
-        gauge={
-            'axis': {'range': [0, 100]},
-            'bar': {'color': bar_color},
-            'steps': [
-                {'range': [0, 30],  'color': '#ffcccc'},   # light red
-                {'range': [30, 60], 'color': '#ffe5b4'},   # light orange
-                {'range': [60, 100],'color': '#ccffcc'},   # light green
-                ],
-            'threshold': {
-                'line': {'color': needle_color, 'width': 4},
-                'thickness': 0.75,
-                'value': readiness_percent
-        }
-    }
-))
+        title={'text':"Readiness %"},
+        gauge={'axis':{'range':[0,100]}}
+    ))
 
-st.plotly_chart(gauge)
+    st.plotly_chart(gauge)
 
     # ---------- Wealth graph ----------
         st.subheader("Retirement Wealth Projection")
