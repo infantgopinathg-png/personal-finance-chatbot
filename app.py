5b47c903708f198141b57ef5bfc1a78c128dcd49
infantgopinathg-png <infantgopinathg@gmail.com>
2026-03-18 12:37:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,14 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
+# CSS
 st.markdown("""
 <style>
+
+.stApp{
+background: linear-gradient(135deg,#0f172a,#1e293b);
+color:white;
+}
 
 .header-card{
 background: rgba(255,255,255,0.05);
@@ -29,21 +35,11 @@ background: linear-gradient(90deg,#22c55e,#38bdf8);
 -webkit-text-fill-color:transparent;
 }
 
-.subtitle{
-font-size:18px;
-color:#cbd5f5;
-margin-top:10px;
-}
-
-.dev{
-font-size:14px;
-color:#94a3b8;
-margin-top:8px;
-}
-
 </style>
 """, unsafe_allow_html=True)
 
+
+# HEADER
 st.markdown("""
 <div class="header-card">
 
@@ -51,11 +47,11 @@ st.markdown("""
 💰 Personal Financial Discipline Advisor
 </div>
 
-<div class="subtitle">
+<div style="font-size:18px;color:#cbd5f5;">
 AI-Based Personal Wealth Planning Tool
 </div>
 
-<div class="dev">
+<div style="font-size:14px;color:#94a3b8;margin-top:10px;">
 Designed & Developed by – Ambika, Infant, Madhushree (AIM)
 </div>
 
