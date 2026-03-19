f5d63cfbc14b394c149b30a965ff0cea1e2cb080
infantgopinathg-png <infantgopinathg@gmail.com>
2026-03-19 12:19:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def government_schemes(age):
 
 
 # ---------- User Inputs ----------
-st.markdown("<h2>User Financial Information</h2>", unsafe_allow_html=True)
+st.header("User Financial Information")
 
 age = st.number_input("Current Age", min_value=18, max_value=100)
 gender = st.radio("Gender", ["Male","Female"])
